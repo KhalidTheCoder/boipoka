7d09efbb41ca6ecf46ca2dedc075c61c24ce0549
Khalidahsan007 <khalidasan2407@gmail.com>
2025-06-13 08:10:20 +0600

    added toastify

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
+import { addToStoreDB } from "../../utility/addToDB";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// const MySwal = withReactContent(Swal);
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -7,12 +14,30 @@ const BookDetails = () => {
   const data = useLoaderData();
   const singleBook = data.find((book) => book.bookId === bookId);
   const { bookName, image } = singleBook;
+  const handleMarkAsRead = (id) => {
+    // Swal.fire({
+    //   position: "center",
+    //   icon: "success",
+    //   title: "Your work has been saved",
+    //   showConfirmButton: false,
+    //   timer: 1500,
+    // });
+    toast("Wow so easy!");
+    addToStoreDB(id);
+  };
   return (
     <div className="w-2/3 mx-auto">
       <img className="w-48" src={image} alt="" />
       <h5>{bookName}</h5>
-      <button className="btn btn-success m-2">Read</button>
-      <button className="btn btn-primary m-2">wish List</button>
+      <ToastContainer />
+      <button
+        onClick={() => handleMarkAsRead(id)}
+        className="btn btn-success mr-2"
+      >
+        Mark As Read
+      </button>
+
+      <button className="btn btn-primary ml-2">Add To Wish List</button>
     </div>
   );
 };
